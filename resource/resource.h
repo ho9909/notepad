67ca1222f103e2d61ac10d32ff15a55cce080b3c
Seong In Jo <85017429+ho9909@users.noreply.github.com>
2025-04-26 18:57:15 +0900

    Update and rename resource.h to resource/resource.h

---
+++ resource/resource.h
@@ -0,0 +1,21 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 메모장.rc
+//
+#define IDR_MENU1                       101
+
+#define ID_KHS_SAVE                     40002
+#define ID_KHS_NEW                         40005
+#define ID_KHS_QUIT                     40006
+#define ID_KHS_OPEN                     40007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40008
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
